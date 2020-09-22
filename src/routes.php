32ccea103a41545e01159768ad72eda0b0719a67
Elan Ruusamäe <glen@pld-linux.org>
2020-09-22 06:14:36 +0300

    Update name -> setName change

--- src/routes.php
+++ src/routes.php
@@ -34,107 +34,107 @@ $app->error(static function (Exception $e) use ($di, $app) {
 $app->get('/', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->index();
-})->name('home');
+})->setName('home');
 
 $app->get('/run/view', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->view();
-})->name('run.view');
+})->setName('run.view');
 
 $app->get('/run/delete', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->deleteForm();
-})->name('run.delete.form');
+})->setName('run.delete.form');
 
 $app->post('/run/delete', static function () use ($di, $app) {
     $di['runController']->deleteSubmit();
-})->name('run.delete.submit');
+})->setName('run.delete.submit');
 
 $app->get('/run/delete_all', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->deleteAllForm();
-})->name('run.deleteAll.form');
+})->setName('run.deleteAll.form');
 
 $app->post('/run/delete_all', static function () use ($di, $app) {
     $di['runController']->deleteAllSubmit();
-})->name('run.deleteAll.submit');
+})->setName('run.deleteAll.submit');
 
 $app->get('/url/view', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->url();
-})->name('url.view');
+})->setName('url.view');
 
 $app->get('/run/compare', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->compare();
-})->name('run.compare');
+})->setName('run.compare');
 
 $app->get('/run/symbol', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->symbol();
-})->name('run.symbol');
+})->setName('run.symbol');
 
 $app->get('/run/symbol/short', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->symbolShort();
-})->name('run.symbol-short');
+})->setName('run.symbol-short');
 
 $app->get('/run/callgraph', static function () use ($di, $app) {
     $app->controller = $di['runController'];
     $app->controller->callgraph();
-})->name('run.callgraph');
+})->setName('run.callgraph');
 
 $app->get('/run/callgraph/data', static function () use ($di, $app) {
     $di['runController']->callgraphData();
-})->name('run.callgraph.data');
+})->setName('run.callgraph.data');
 
 $app->get('/run/callgraph/dot', static function () use ($di, $app) {
     $di['runController']->callgraphDataDot();
-})->name('run.callgraph.dot');
+})->setName('run.callgraph.dot');
 
 // Import route
 $app->post('/run/import', static function () use ($di, $app) {
     /** @var ImportController $controller */
     $controller = $di['importController'];
     $controller->import();
-})->name('run.import');
+})->setName('run.import');
 
 // Watch function routes.
 $app->get('/watch', static function () use ($di, $app) {
     $app->controller = $di['watchController'];
     $app->controller->get();
-})->name('watch.list');
+})->setName('watch.list');
 
 $app->post('/watch', static function () use ($di) {
     $di['watchController']->post();
-})->name('watch.save');
+})->setName('watch.save');
 
 // Custom report routes.
 $app->get('/custom', static function () use ($di, $app) {
     $app->controller = $di['customController'];
     $app->controller->get();
-})->name('custom.view');
+})->setName('custom.view');
 
 $app->get('/custom/help', static function () use ($di, $app) {
     $app->controller = $di['customController'];
     $app->controller->help();
-})->name('custom.help');
+})->setName('custom.help');
 
 $app->post('/custom/query', static function () use ($di) {
     $di['customController']->query();
-})->name('custom.query');
+})->setName('custom.query');
 
 // Waterfall routes
 $app->get('/waterfall', static function () use ($di, $app) {
     $app->controller = $di['waterfallController'];
     $app->controller->index();
-})->name('waterfall.list');
+})->setName('waterfall.list');
 
 $app->get('/waterfall/data', static function () use ($di) {
     $di['waterfallController']->query();
-})->name('waterfall.data');
+})->setName('waterfall.data');
 
 // Metrics
 $app->get('/metrics', static function () use ($di, $app) {
     $di['metricsController']->metrics();
-})->name('metrics');
+})->setName('metrics');
